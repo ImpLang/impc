f5fa1c508d0072da84e3306ae05883d95250bd3f
HXM4Tech <68482946+HXM4Tech@users.noreply.github.com>
2023-03-18 18:25:01 +0100

    Changed setup.sh script to use printf command instead of echo

--- setup.sh
+++ setup.sh
@@ -12,11 +12,11 @@ pip install -r requirements.txt
 ln -svf src/main.py impc
 chmod +x impc
 
-echo "\n"
-echo "Done. You can now run compiler with ./impc"
-echo "\n"
-echo "If you want to use it system-wide, run following commands:"
-echo "sudo mkdir -pv /opt/ImpLang"
-echo "sudo cp -R <project-path>/* /opt/ImpLang/"
-echo "sudo chown -R root:root /opt/ImpLang/"
-echo "sudo ln -svf /opt/ImpLang/impc /usr/local/bin/impc"
+printf "\n"
+printf "Done. You can now run compiler with ./impc\n"
+printf "\n"
+printf "If you want to use it system-wide, run following commands:\n"
+printf "sudo mkdir -pv /opt/ImpLang\n"
+printf "sudo cp -R ./* /opt/ImpLang/\n"
+printf "sudo chown -R root:root /opt/ImpLang/\n"
+printf "sudo ln -svf /opt/ImpLang/impc /usr/local/bin/impc\n"
